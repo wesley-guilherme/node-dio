5948758e7259109ae5f57b5ed51141ebbcba7dbb
Wesley <wesley.analistasystem@gmail.com>
2025-08-19 23:06:10 -0300

    commit not  git parei na aula password create

--- project-qrcode/src/prompts/prompt-main.js
+++ project-qrcode/src/prompts/prompt-main.js
@@ -1,9 +1,13 @@
+import chalk from "chalk"
+
 const mainPrompt = [
     {
         name: "select",
-        description: "Escolha a ferramente (1 -QRCODE ou 2- PASSWORD",
+        description: chalk.yellowBright.bold( "Escolha a ferramenta 1 -QRCODE ou 2- PASSWORD"),
         pattern: /^[1-2]+$/,
-        message: "Escolha apenas entre 1 e 2",
+        message: chalk.redBright.italic("Escolha apenas entre 1 e 2"),
         required: true,
     },
 ];
+
+export default mainPrompt;
---
+++ project-qrcode/src/prompts/prompt-qrcode.js
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+import prompt from "prompt";
+
+const promptQRCode = [
+ {
+    name: "link",
+    description: chalk.yellow("Digite o link para gerar o QR CODE"),
+ },
+ {
+    name:"type",
+    description: chalk.yellow(
+        " Escolha entre o tipo de QRcode (1-Normal ou ( 2- Terminal"
+    ),
+    pattern: /^[1-2]+$/,
+    message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+    required: true,
+ }
+];
+
+export default promptQRCode;
