da971ce1b8964124b642897942dbb1ee03efd849
Wesley <wesley.analistasystem@gmail.com>
2025-09-04 21:50:24 -0300

    commit projeto fórmula 1

---
+++ project-f1/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+import { request } from "http";
+
+const server = fastify({logger: true});
+
+server.get("/teams", async (request, response) => {
+    response.type("application/json").code(200);
+    return [{id: 1, name:"ferrari"}];
+});
+
+server.listen({port: 3333}, () =>{
+    console.log("Server Init");
+})
